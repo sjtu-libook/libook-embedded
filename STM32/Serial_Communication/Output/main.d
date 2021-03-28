a125f22ca084b12266332dd4015bf3abddd80b64
yangco-le <yanglily@sjtu.edu.cn>
2021-03-28 17:26:53 +0800

    Communication

---
+++ STM32/Serial_Communication/Output/main.d
@@ -0,0 +1,13 @@
+..\..\output\main.o: ..\..\User\main.c
+..\..\output\main.o: ..\..\Libraries\CMSIS\stm32f10x.h
+..\..\output\main.o: ..\..\Libraries\CMSIS\core_cm3.h
+..\..\output\main.o: D:\software\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+..\..\output\main.o: ..\..\Libraries\CMSIS\system_stm32f10x.h
+..\..\output\main.o: ..\..\User\stm32f10x_conf.h
+..\..\output\main.o: ..\..\Libraries\FWlib\inc\stm32f10x_gpio.h
+..\..\output\main.o: ..\..\Libraries\CMSIS\stm32f10x.h
+..\..\output\main.o: ..\..\Libraries\FWlib\inc\stm32f10x_rcc.h
+..\..\output\main.o: ..\..\Libraries\FWlib\inc\stm32f10x_usart.h
+..\..\output\main.o: ..\..\Libraries\FWlib\inc\misc.h
+..\..\output\main.o: ..\..\User\usart\bsp_usart.h
+..\..\output\main.o: D:\software\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
